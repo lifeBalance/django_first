ad986ff09d50b262f835a39a0213f08fff6e7129
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-25 22:33:23 +0300

    create Topic model (models.py), makemigrations, and migrate

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'learning_log_app',
+    # Default Django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
