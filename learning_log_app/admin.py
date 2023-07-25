3120dd6aebc323e4e3cabca02a239d1cd45c9974
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-25 22:51:11 +0300

    Add Entry model, register it. Run migrations.

--- learning_log_app/admin.py
+++ learning_log_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Import the model we want to register.
 # The dot (.models) is bc models.py is in the same directory as admin.py.
-from .models import Topic
+from .models import Topic, Entry
 
 # Register your models here.
 admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_log_app/models.py
+++ learning_log_app/models.py
@@ -1,11 +1,31 @@
+from tabnanny import verbose
 from django.db import models
+
 
 # Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about."""
+
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # This nested class (some weird shit) allows us to define the plural of
+    # Entry as Entries and not Entrys (that would be the default).
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Return a simple string representation of the entry."""
+        return f"{self.text[:50]}..."  # Just the first 50 characters.
