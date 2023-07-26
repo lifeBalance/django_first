9d7254207b77f4b0f84bbce1b3d36674cb29e03e
javi <3051496+lifeBalance@users.noreply.github.com>
2023-07-26 22:43:48 +0300

    create home route/view/template

---
+++ learning_log_app/urls.py
@@ -0,0 +1,13 @@
+"""Defines URL patterns for learning_log_app."""
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_log_app'
+urlpatterns = [
+    # Home page. The function path takes in 3 arguments:
+    # 1. The URL (Base URL is ignored: http://localhost:8000)
+    # 2. The module.function with the controller (views.py, index function)
+    # 3. A handy name that we can use in links to this route.
+    path('', views.index, name='index'),
+]
--- learning_log_app/views.py
+++ learning_log_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here (views roughly correspond to controllers in MVC).
+
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_log_app/index.html')
