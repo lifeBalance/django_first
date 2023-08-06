d5876c86d55a0ee13ffdf0466924ca008bc8eeef
javi <3051496+lifeBalance@users.noreply.github.com>
2023-08-06 23:51:41 +0300

    Protecting topics

--- learning_log_app/views.py
+++ learning_log_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -17,7 +18,7 @@ def index(request):
 def topics(request):
     """Show all topics."""
     # Pull the list of topics from the model.
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     # Define a context dictionary, and hang the topics there as an attribute.
     context = {"topics": topics}
     return render(request, "learning_log_app/topics.html", context)
@@ -28,6 +29,9 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     # Query to select a topic using the value captured in the url (/<int:topic_id>/)
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
     # Query to get all the entries in the topic, ordered by date.
     entries = topic.entry_set.order_by("-date_added")
     # Build a context dictionary with the topic and its list of entries.
@@ -47,7 +51,10 @@ def new_topic(request):
         # Validate that all fields are present, and that they conform to
         # the attributes of the Topic model (text max. length 200 chars.)
         if form.is_valid():
-            form.save()
+            # We need to add ownership to topic before saving it to DB
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("learning_log_app:topics")
 
     # If we didn't return in the else > if clause, display a blank or invalid form.
@@ -84,6 +91,9 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic # Store the topic of the entry in 'topic'
+    # Protect the edit_entry page so that only owner can edit her topics.
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # For GET requests, pre-fill the form with the current entry.
